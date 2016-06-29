e1e8e2df7159f5d2f31f8fed6a7b6066dd081d0e
nihau <gorbach.nikita@ya.ru>
2016-06-29 15:39:22 +0300

    what's that, lol?

--- zmeyka-src/array.js
+++ zmeyka-src/array.js
@@ -2,24 +2,12 @@ Array.prototype.contains = function(item) {
 	return this.indexOf(item) !== -1 
 }
 
-Array.prototype.first = function (item) {
-	if (item === undefined) {
-		return this[0];
-	} else if (typeof(item) === 'function') {
-		item(this[0]);
-	}	
-
-	this[0] = item;
+Array.prototype.first = function () {
+	return this[0];
 }
 
 Array.prototype.last = function (item) {
-	if (item === undefined) {
-		return this[this.length - 1];
-	} else if (typeof(item) === 'function') {
-		item(this[this.length - 1]);
-	}
-
-	this[this.length - 1] = item;
+	return this[this.length - 1];
 }
 
 Array.prototype.remove = function (item) {
